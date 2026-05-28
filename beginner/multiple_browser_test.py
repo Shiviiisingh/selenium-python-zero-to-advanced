ddfea8b83b2bf15013fdf82ec67f090dd865141c
Shiviiisingh <shiviiisingh1406@gmail.com>
2026-05-28 23:20:14 +0530

    add time.sleep()

--- beginner/multiple_browser_test.py
+++ beginner/multiple_browser_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time #added to allow us to pause the script and see the browser actions
 
 #Store the URLs of the website we want to visit in a list
 sites = [
@@ -11,6 +12,7 @@ driver = webdriver.Chrome()
 # loop through the list of URLs and navigate to each one, printing the title of the page
 for site in sites:
     driver.get(site)
+    time.sleep(2)  # Wait for the page to load but not recommended for production code, better to use explicit waits
     print(driver.title)
     
 driver.quit()
